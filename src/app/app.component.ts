44e89b231bfa2e2612aad44b13da11748f3643d8
defofotg <defo.georges@gmail.com>
2018-07-22 19:37:22 +0200

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { timer} from "rxjs/observable/timer";
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  pages: Array<{title: string, component: any}>;
+
+  showSplash = true;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
+              private translate: TranslateService) {
+
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      timer(3000).subscribe(() => this.showSplash = false);
+
+      this.translate.addLangs(["fr", "en"]);
+      this.translate.setDefaultLang('fr');
+
+      let browserLang = this.translate.getBrowserLang();
+      this.translate.use(browserLang.match(/fr|en/) ? browserLang : 'fr');
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
